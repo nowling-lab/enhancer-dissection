d6500c55ca26d684e3dc7002f6da62faa66aab9a
John Peters <petersjg@msoe.edu>
2021-09-16 18:49:34 -0500

    working html generator from 2 fimo tsv and fasta file. Note, this is not a command line program so it requires changing the file_paths in main to run

--- html_generator.py
+++ html_generator.py
@@ -1,9 +1,10 @@
 from os import read
 
 def main():
-    motif_dict = read_fimo_file('./temp_files_pre_params/enhancer-dissection-out/FIMO_16_enhancers/fimo.tsv')
-    seq_dict = read_fasta_file('./temp_files_pre_params/16_enhancers.fa')
-    html_string = generate_html(motif_dict, seq_dict)
+    motif_dict = read_fimo_file('./temp_files_pre_params/enhancer-dissection-out/FIMO_Nardini/fimo.tsv')
+    motif_dict2 = read_fimo_file('./temp_files_pre_params/enhancer-dissection-out/FIMO_Nardini_stremetxt/fimo.tsv')
+    seq_dict = read_fasta_file('./temp_files_pre_params/nardini_luciferase_fragments.fasta')
+    html_string = generate_html(motif_dict, motif_dict2, seq_dict)
     html_string_to_output(html_string, './test.html')
 
 def html_string_to_output(html_string, outputdir):
@@ -11,7 +12,7 @@ def html_string_to_output(html_string, outputdir):
     output.write(html_string)
     output.close
 
-def generate_html(motif_dict, seq_dict):
+def generate_html(motif_dict1, motif_dict2, seq_dict):
     html_string = """
     <!DOCTYPE html>
     <html lang="en-US">
@@ -25,70 +26,71 @@ def generate_html(motif_dict, seq_dict):
           content="This is a highlighted FIMO output thingy">
     </head>
     <body>
+    <h> Legend: </h>
+    <div style="background:Aqua; width:75%;"> This is for fimo run with streme.txt as the motif file </div>
+    <div style="background:PaleVioletRed; width:75%"> This is for fimo run with JASPAR as the motif file </div>
+    <div style="background:Plum; width:75%"> This is for when those highlighted motifs overlap </div>
      """
-    before_tag = '<span style="background-color:DodgerBlue">'
-    after_tag = '</span>'
-    tag_length = len(before_tag) + len(after_tag)
     dynamic_html_string = None
-    motif_keys = list(motif_dict.keys())
+    motif_keys = list(motif_dict1.keys())
     motif_keys.sort()
     for key in motif_keys:
         if dynamic_html_string == None:
-            dynamic_html_string = "<p>\n"
+            dynamic_html_string = """<p style="font-family:'Courier New'; word-wrap: break-word; width: 75%">"""
         else:
-            dynamic_html_string += "<p>\n"
-        dynamic_html_string += ">" + str(key) + "\n"
+            dynamic_html_string += """<p style="font-family:'Courier New'; word-wrap: break-word; width: 75%">"""
+        dynamic_html_string += ">" + str(key) + "<br>"
 
         sequence = seq_dict[key]
-        highlight_list = motif_dict[key]
+        highlight_list = motif_dict1[key]
         highlight_list = sorted(highlight_list, key=lambda x: x[0])
-        highlight_list2 = clean_overlapping_motifs(highlight_list)
-        #print(highlight_list, "\n\n")
-        #print(highlight_list2, "\n\n")
-        previous_length = None
-        for index, highlight in enumerate(highlight_list):
-            start, end = highlight
-            end = end - 1
-            if index > 0:
-                motif_width = end - start + 1
-                dist_to_next_motif = start - highlight_list[index -1][1]
-                next_motif_start = previous_length + dist_to_next_motif
-                next_motif_end = motif_width + next_motif_start
+        overlap_dict = {}
+        
+        highlight_overlaps(highlight_list, overlap_dict, "blue")
 
-                seq_start = sequence[:next_motif_start]
-                seq_motif = sequence[next_motif_start - 1: next_motif_end]
-                seq_end = sequence[next_motif_end :]
-                sequence = seq_start + before_tag + seq_motif + after_tag + seq_end
-                previous_length = len(seq_start) + len(seq_motif) + tag_length
+        highlight_list2 = motif_dict2[key]
+        highlight_list2 = sorted(highlight_list2, key=lambda x: x[0])
+        
+        highlight_overlaps(highlight_list2, overlap_dict, "red")
+
+        for index, char in enumerate(sequence):
+            index_1_based = index + 1
+            if index_1_based in overlap_dict:
+                color = overlap_dict[index_1_based]
             else:
-                seq_start = sequence[:start]
-                seq_motif = sequence[start - 1: end]
-                seq_end = sequence[end:]
-                sequence = seq_start + before_tag + seq_motif + after_tag + seq_end
-                previous_length = len(seq_start) + len(seq_motif) + tag_length
-        dynamic_html_string += sequence
+                color = "none"
+
+            if color == "purple":
+                dynamic_html_string += '<span style="background:Plum;">'
+                dynamic_html_string += char.upper()
+                dynamic_html_string += "</span>"
+            elif color == "red":
+                dynamic_html_string += '<span style="background:PaleVioletRed;">'
+                dynamic_html_string += char.upper()
+                dynamic_html_string += "</span>"
+            elif color == "blue":
+                dynamic_html_string += '<span style="background:Aqua;">'
+                dynamic_html_string += char.upper()
+                dynamic_html_string += "</span>"
+            else:
+                dynamic_html_string += char.upper()
+
         dynamic_html_string += "\n</p>"
 
     html_string += dynamic_html_string
     html_string += '\n</body>'
     return html_string
 
-def clean_overlapping_motifs(highlight_list):
-    new_list = []
-    for index, high_light in enumerate(highlight_list):
-        end = None
-        for x in range(index + 1, len(highlight_list)):
-            if high_light[1] >= highlight_list[x][0]:
-                end = highlight_list[x][1]
-            else:
-                break
-        if end is not None:
-            new_list.append((high_light[0], end))
-        else:
-            print(new_list[-1][1])
-            if new_list[-1][1] != high_light[1]:
-                new_list.append(high_light)
-    return sorted(new_list, key=lambda x: x[0])
+def highlight_overlaps(highlight_list, overlap_dict, color):
+    for highlight in highlight_list:
+            start, end = highlight
+            for x in range(int(start), int(end) + 1):
+                if x in overlap_dict:
+                    ch_color = overlap_dict[x]
+                    if color != ch_color:
+                        overlap_dict[x] = "purple"
+                else:
+                    overlap_dict[x] = color
 
 def read_fasta_file(file_path):
     sequence_dictionary = {}
@@ -104,7 +106,7 @@ def read_fasta_file(file_path):
                     sequence_name = split_header[0]
                     sequence_name = sequence_name[1:]
                 else:
-                    sequence_dictionary[int(sequence_name)] = sequence
+                    sequence_dictionary[sequence_name] = sequence
                     sequence_name = split_header[0]
                     sequence_name = sequence_name[1:]
                     sequence = None
@@ -113,7 +115,7 @@ def read_fasta_file(file_path):
                     sequence = line.strip()
                 else:
                     sequence += line.strip() 
-    sequence_dictionary[int(sequence_name)] = sequence
+    sequence_dictionary[sequence_name] = sequence
     return sequence_dictionary
 
 def read_fimo_file(file_path):
@@ -124,12 +126,13 @@ def read_fimo_file(file_path):
                 if len(line) <= 1:
                     return sequence_name_dict
                 line_split = line.split()
-                seq_name = int(line_split[2])
-                start = int(line_split[3])
-                stop = int(line_split[4])
+                seq_name = line_split[2]
+                start = line_split[3]
+                stop = line_split[4]
 
                 if seq_name not in sequence_name_dict:
                     sequence_name_dict[seq_name] = []
+                    sequence_name_dict[seq_name].append((start, stop))
                 else:
                     sequence_name_dict[seq_name].append((start, stop))  
     return sequence_name_dict
