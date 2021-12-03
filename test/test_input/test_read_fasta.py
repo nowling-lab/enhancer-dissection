dc4e01f551abb2eac7759868e24ebe4585e7ad36
John Peters <petersjg@msoe.edu>
2021-12-02 23:00:42 -0600

    okay am actually done for tonight. I got pytest working which is nice. Though it's weird with that sys path stuff

--- test/test_input/test_read_fasta.py
+++ test/test_input/test_read_fasta.py
@@ -1,14 +1,14 @@
 import sys
 import os
+import pytest
 
 sys.path.insert(0, os.getcwd())
 
 from clustal_highlighter.clustal_format_highlighter import read_fasta_file
 
-def read_fasta_test():
+@pytest.mark.sanity
+def test_read_fasta():
     fasta = read_fasta_file('test/test_input/input_files/test_fasta.fa', False)
-    print(fasta)
-    #assert fasta == "hi"
-    
-read_fasta_test()
+    assert fasta == "hi"
+
     
